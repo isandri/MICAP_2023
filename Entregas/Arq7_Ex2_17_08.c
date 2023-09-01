11c6acfb2fb6cdf31b49baeeacb1a5da7536227c
Mateus B. Cassiano <mbc07@live.com>
2023-09-01 19:48:43 -0400

    Exercício 3 e 6 do Arq 6, exercício 2 do Arq 9

--- Entregas/Arq7_Ex2_17_08.c
+++ Entregas/Arq7_Ex2_17_08.c
@@ -8,14 +8,11 @@
 #define DISP_E P1_0
 #define DISP_D P1_1
 #define SEGS P0
-#define PONTO P0_7
-#define LIG 0
-#define DES 1
-
-__code unsigned char caract[13] = {0x7E, 0x30, 0x6D, 0x79,
-                                   0x33, 0x5B, 0x5F, 0x70,
-                                   0x7F, 0x7B, 0x00, 0xFF,
-                                   0x4F};
+#define PONT P0_7
+								//   0     1     2     3     4     5     6
+__code unsigned char caract[13] = {0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 
+								   0x70, 0x7f, 0x7b, 0x00, 0xff, 0x4f};
+								//   7     8     9           8.    E
 
 unsigned char disp1;
 unsigned char disp2;
@@ -23,13 +20,15 @@ unsigned char select=1;
 
 
 void delay_ms(unsigned int t){
-	TMOD |= 0x01;
+	TMOD |=  0x01;
 	TMOD &= ~0x02;
+
 	for (;t>0;t--){
 		TR0 = 0;
 		TF0 = 0;
 		TL0 = 0x58;
-		TH0 = 0x9E;
+		TH0 = 0x9e;
+
 		TR0 = 1;
 		while(TF0==0);
 	}
@@ -37,33 +36,32 @@ void delay_ms(unsigned int t){
 
 void funcao_isr (void) __interrupt 5 {
 	if(select == 1){
-		P1_1=1;
-		P1_0=0;
-		P0=caract[disp1];
-		select=2;
+		DISP_E = 0;
+		DISP_D = 1;
+		SEGS   = caract[disp1];
+		select = 2;
 	}
 	else if(select == 2){
-		P1_0=1;
-		P1_1=0;
-		P0=caract[disp2];
-		select=1;
+		DISP_E = 1;
+		DISP_D = 0;
+		SEGS   = caract[disp2];
+		select = 1;
 	}
 }
 
 
-void esc_disp7s(unsigned int num_vis){
-	disp1 = num_vis/10;
-	disp2 = num_vis%10;
-	
+void esc_disp7s(unsigned int num_vis) {
+	disp1 = num_vis / 10;
+	disp2 = num_vis % 10;
 }
 
 void main (void){
-	int i;
+	unsigned int i;
+
 	Init_Device();
 	SFRPAGE=LEGACY_PAGE;
-	
 
-	for(i=0; i<100;i++){
+	for(i=0; i<100; i++) {
 		esc_disp7s(i);
 		delay_ms(500);
 	}
